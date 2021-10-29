4932224adc7395364bcdf545ee2cf802f4b547bd
Brieona <brieona.turner@gmail.com>
2021-10-29 16:05:17 -0400

    Adding further cleaning as well as start of the postgres query

---
+++ Postgres_Query.sql
@@ -0,0 +1,28 @@
+/*  Below are selections to make sure that the data has loaded in properly. */
+
+SELECT * FROM "Movies";
+SELECT * FROM "Oscars";
+
+/* Joining Tables to create a new one */
+
+CREATE TABLE Combination_Table AS
+SELECT m.budget, m.release_year, m.revenue, m.runtime, m.title, m.genres,
+		o.film, o.winner, o.category
+	FROM "Movies" m
+	JOIN "Oscars" o
+		ON m.title = o.film;
+		
+/* Checking Table */
+SELECT * FROM Combination_Table;
+
+
+/* Getting Distinct IDs*/
+SELECT DISTINCT ON (film) * FROM combination_Table
+
+/*Deleting Duplicates for films*/
+
+
+
+
+  
+  
