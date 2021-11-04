2fa11bb16deefb69c4bd2ad79fef6161a1c61678
Brieona <brieona.turner@gmail.com>
2021-11-04 17:46:30 -0400

    Editing query and cleaning

--- Postgres_Query.sql
+++ Postgres_Query.sql
@@ -6,17 +6,17 @@ SELECT * FROM "Oscars";
 /* Joining Tables to create a new one */
 
 CREATE TABLE Combination_Table AS
-SELECT m.budget, m.release_year, m.revenue, m.runtime, m.title, m.genres,
+SELECT m.budget, m.release_year, m.revenue, m.runtime, m.title, m.genres, m.nominated,
 		o.film, o.winner, o.category
 	FROM "Movies" m
 	JOIN "Oscars" o
-		ON m.title = o.film;
-		
+		ON o.film = m.title;
+	
+/* Dropping film column*/
+ALTER TABLE Combination_Table
+DROP COLUMN film;
+
 /* Checking Table */
 SELECT * FROM Combination_Table;
 
-
-
-
-  
   
