902602b5c5e396de2a6d2b888d51a51dafcbd5f3
Brieona <brieona.turner@gmail.com>
2021-11-14 17:44:39 -0500

    Updating Query and Table

--- Postgres_Query.sql
+++ Postgres_Query.sql
@@ -8,7 +8,7 @@ SELECT * FROM "Oscars";
 CREATE TABLE Combination_Table AS
 SELECT m.release_year, m.title, m.genres, m.nominated, m.budget_ranges, 
 		m.revenue_ranges, m.runtime_ranges,
-		o.film, o.winner
+		o.film, o.winner, o.category
 	FROM "Movies" m
 	JOIN "Oscars" o
 		ON o.film = m.title;
@@ -26,3 +26,22 @@ GROUP BY genres;
 
 /*Incase of needing to drop table...*/
 DROP TABLE Combination_Table
+
+
+
+
+/* Combo table V2*/
+CREATE TABLE Combination_Table_V2 AS
+SELECT m.release_year, m.title, m.genres, m.nominated, m.budget_ranges, 
+		m.revenue_ranges, m.runtime_ranges,
+		o.film, o.winner , o.category
+	FROM "Oscars" o
+	LEFT JOIN "Movies" m
+        ON m.title = o.film;
+		
+	ALTER TABLE Combination_Table_V2
+	DROP COLUMN film;
+	
+	
+/* Checking Table */
+SELECT * FROM Combination_Table_V2;
